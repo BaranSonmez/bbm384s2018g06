98d3abc714e7c80db60a1afb1cee7084ae982e12
mervekantarci <32953637+mervekantarci@users.noreply.github.com>
2018-04-09 14:05:52 +0300

    SpringLocalhost
    Only trainer table added works on localhost.

---
+++ Spring/complete/src/main/java/TrainerPackage/TrainerRepository.java
@@ -0,0 +1,7 @@
+package TrainerPackage;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TrainerRepository extends  CrudRepository<trainer, Long> {
+
+}
---
+++ Spring/complete/src/main/java/TrainerPackage/trainer.java
@@ -0,0 +1,63 @@
+package TrainerPackage;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+
+
+
+@Entity
+public class trainer {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String name;
+	private String surname;
+	private String username;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+	
+	public trainer() {
+		
+	}
+	
+	public trainer(String name, String surname, String username, String password) {
+		this.name = name;
+		this.surname = surname;
+		this.username = username;
+		this.password = password;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
